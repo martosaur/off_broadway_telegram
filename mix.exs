4d20f9e72d777e2b6bd42d7c9c76d388dfcba307
martosaur <martosaur@gmail.com>
2026-06-16 15:22:11 -0700

    2.0.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OffBroadway.Telegram.MixProject do
   use Mix.Project
 
-  @version "2.0.0"
+  @version "2.0.1"
   @description "Off-Broadway producer for Telegram Bot API"
 
   def project do
